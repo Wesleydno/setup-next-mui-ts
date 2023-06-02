26f744f31b0e5320ab34a729a80086c38f32104b
Wesley Reis <wesleydno2011@gmail.com>
2023-06-02 09:36:46 -0300

    feat: add erro pages

---
+++ pages/500.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+export default function Error() {
+  return (
+    <Box component="header">
+      <Typography variant="h4" component="h1" gutterBottom>
+        Error 500
+      </Typography>
+    </Box>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,15 @@ export default function Home() {
       <Typography variant="h4" component="h1" gutterBottom>
         Home
       </Typography>
+      <Box component="section">
+        <Typography variant="subtitle1" component="h2" gutterBottom>
+          Something short and leading about the collection below—its contents,
+          the creator, etc. Make it short and sweet, but not too short so folks
+          don't simply skip over it entirely.
+        </Typography>
+        <Typography>Paragrafo 1</Typography>
+        <Typography>Paragrafo 2</Typography>
+      </Box>
     </Box>
   );
 }
